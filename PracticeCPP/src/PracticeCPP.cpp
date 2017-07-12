5d20c4a582554e61ee0dcfbad503c429bd60aeed
TMCNguyen-PC\TMCNguyen <nguyen.tmc0761@gmail.com>
2017-07-12 18:00:48 +0700

    First commit

---
+++ PracticeCPP/src/PracticeCPP.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : PracticeCPP.cpp
+// Author      : NguyenTMC
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
